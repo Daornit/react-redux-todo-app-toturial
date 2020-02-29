7c523cfbb57c34ea23112b413ba99c583d747882
Daornit <daornit2@gmail.com>
2020-02-29 22:21:24 +0800

    test commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 render(
   <Provider store={store} >
     <App/>
+    
   </Provider>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
